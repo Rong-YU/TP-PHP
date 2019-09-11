1915c73ee5b5e457f84e9f06989a023ee9873dee
Rong YU <11800323@crit-xubuntu>
2019-09-11 17:14:21 +0200

    tp2

--- TODOlist.php
+++ TODOlist.php
@@ -5,8 +5,16 @@ class Foo {
     function __construct() {
       $this->to_dos = [];
     }
-    function aMemberFunc() {
-        print 'Inside `aMemberFunc()`';
+    function add_to_do($chaine) {
+        if (empty(trim($chaine))){
+          $to_dos[]=$chaine;
+        }
+        else{
+          echo 'la chaine est vide ou ne contient que des espaces';
+        }
+    }
+    function remove_to_do($i){
+      unset($to_dos[$i]);
     }
 }
 
